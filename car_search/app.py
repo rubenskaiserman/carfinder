c33bd2169f22b93294ef1561d2247e186df1cdb7
rubskaiserman <rubskaiserman@gmail.com>
2022-04-16 15:38:06 -0300

    Iniciando preparações iniciais

--- car_search/app.py
+++ car_search/app.py
@@ -1,2 +1,10 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 
