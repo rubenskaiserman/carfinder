014478a6fb221ee67e338305afc40337458eacbd
rubskaiserman <rubskaiserman@gmail.com>
2022-05-09 09:21:24 -0300

    Inicio da api

--- website_stuff/carfinder/app.py
+++ website_stuff/carfinder/app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template, request
+# from flask import Flask, request, jsonify
+import psycopg
+import json
+# import psycopg.extras
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/', methods=["POST"])
+# def index():
+
+def get_request(id:str):
+    with psycopg.connect("dbname=car_search user=kaiserman password=chakalaka") as conn:
+        with conn.cursor() as cur:
+            cur.execute(f"SELECT * FROM cars WHERE id='{id}'")
+            dict_result = dict()
+            result = cur.fetchall()
+            i = 0
+            for column in cur.description:
+                dict_result[column.name] = result[0][i]
+                i+=1
+            
+            return dict_result
+
+
+teste = json.dumps(get_request('9ecbe8c340a04bb1ab3837c6f9232ff7'), indent=4)
+print(teste)
+    
+            
+
 
 
 
